a939778f6bc6a16acbc181489821fd11e84eeaa1
nicolas <nicolas.signolet@gmail.com>
2020-09-25 00:01:40 +0200

    DefaultController TF

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,13 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-//    public function testIndex()
-//    {
-//        $client = static::createClient();
-//
-//        $crawler = $client->request('GET', '/');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
-//    }
+    use LogTrait;
+
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\KernelBrowser
+     */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testIndexNotLogged()
+    {
+        $this->client->request('GET', '/');
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+
+        $crawler = $this->client->followRedirect();
+        $this->assertSame(2, $crawler->filter('form input#username')->count() + $crawler->filter('form input#password')->count());
+    }
+
+    public function testIndexLogged()
+    {
+        $this->logInUser();
+        $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('Bienvenue sur Todo List', $this->client->getResponse()->getContent());
+    }
 }
